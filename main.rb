207341ee89b3a2ba4f3da847f461a023732d888d
gscarv13 <gsc13@tuta.io>
2020-12-30 14:32:21 -0300

    Create examples for testing

--- main.rb
+++ main.rb
@@ -1,1 +1,17 @@
-#Test comment
+module Enumerable
+  def my_each
+    # your code here
+  end
+end
+
+# test variables
+array_test = [1, 2, 3, 4, 5, 6]
+obj_test = { one: 'hey1', two: 'hey2', three: 'hey3' }
+
+puts 'Original each'
+array_test.each { |e| e**2 }
+obj_test.each { |e| p e }
+
+puts 'Our script'
+array_test.my_each { |e| e**2 }
+obj_test.my_each { |e| p e }
