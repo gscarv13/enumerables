8cf8395541ca5246bb9d92611284aad33c890d17
johnherncode <besthostandseoinfo@gmail.com>
2020-12-30 14:50:32 -0600

    Add my_select method

--- main.rb
+++ main.rb
@@ -21,19 +21,42 @@ module Enumerable
     self
   end
 
+  def my_select
+    result = []
+    my_each { |e| result << e if yield e }
+    result
+  end
+
+  def my_all?
+  end
+
+  def my_any?
+  end
+
+  def my_none?
+  end
+
+  def my_count
+  end
+
+  def my_map
+  end
+
+  def my_inject
+  end
 
 end
 
 # test variables
 array_test = [1, 2, 3, 4, 5, 6]
-obj_test = { one: 'hey1', two: 'hey2', three: 'hey3' }
+obj_test = { one: 1, two: 2, three: 3 }
 
 # Method call for tests each
 puts '------------Original each--------------'
 array_test.each { |e| p e**2 }
 obj_test.each { |e| p e }
 
-puts '-------------This script----------------'
+puts '-------------This script each-----------'
 array_test.my_each { |e| p e**2 }
 obj_test.my_each { |e| p e }
 
@@ -46,4 +69,12 @@ array_test.my_each_with_index { |e, i| p e, i }
 obj_test.my_each_with_index { |e, i| p e, i }
 
 # Method call for tests Select
+puts '-------Original select---------'
+p array_test.select(&:even?)
+p(obj_test.select { |_k, v| v < 2 })
+
+puts '----This script select---------'
+p array_test.my_select(&:even?)
+p(obj_test.my_select { |_k, v| v < 2 })
+
 # Method call for tests all?
